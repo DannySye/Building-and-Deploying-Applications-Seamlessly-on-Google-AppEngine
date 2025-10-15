6c189358dc0f18bd41ba26a80975b81e473186ae
DannySye <syemwayne1122@gmail.com>
2025-10-15 15:11:44 +0300

    flexible fix:

--- flexible-environment-demo/main.py
+++ flexible-environment-demo/main.py
@@ -102,5 +102,5 @@ def reset():
 
 if __name__ == '__main__':
     # This is used when running locally only.
-    app.run(host='127.0.0.1', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
 
